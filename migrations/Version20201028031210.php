e8c48051fcd3924735e5687d5dd41a5ebcb9a1d7
Иосиф "Kening" Лазарев <51381102+Kening1@users.noreply.github.com>
2020-11-19 21:19:07 +0600

    Update Version20201028031210.php

--- migrations/Version20201028031210.php
+++ migrations/Version20201028031210.php
@@ -29,6 +29,6 @@ final class Version20201028031210 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE users');
-        $this->addSql2('DROP TABLE Goods');
+        $this->addSql2('DROP TABLE goods');
     }
 }
