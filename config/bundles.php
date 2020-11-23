5db8ac18bb78f2604dd074343778e4923f7cd350
Иосиф "Kening" Лазарев <51381102+Kening1@users.noreply.github.com>
2020-11-23 19:19:46 +0600

    Add files via upload

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Fresh\DoctrineEnumBundle\FreshDoctrineEnumBundle::class => ['all' => true],
     Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
